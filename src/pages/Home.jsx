b490f10e104ace40de66de6c6bf70d596957d88e
hyeisfree <nahye6559@naver.com>
2024-11-28 22:01:37 +0900

    연습용 커밋입니다.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <p>로그인/회원가입</p>
+        <h1>제목 정해야 함</h1>
+        <p>메뉴바</p>
+      </header>
+      <main>
+        <article>그림 넣을 곳</article>
+      </main>
+      <footer>
+        <p>글</p>
+        <p>글</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
